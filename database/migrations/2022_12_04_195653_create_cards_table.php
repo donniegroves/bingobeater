1d39e97db1d4eb59dfb16913374ff678a8889fce
Donnie Groves <donniegroves83@gmail.com>
2022-12-04 20:20:08 -0500

    cards table

---
+++ database/migrations/2022_12_04_195653_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->integer('game_id');
+            $table->tinyInteger('col');
+            $table->tinyInteger('row');
+            $table->string('artist');
+            $table->string('song_title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+};
